ae6c5301ec1816a4eedabc8e3490e05a93fd745e
Marcelo Santos <masantos.itlean@sbt.com.br>
2020-04-14 14:24:10 -0300

    adding web controller

---
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,32 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+  @RequestMapping("/{beerId}")
+  public ResponseEntity getBeerById(@PathVariable UUID beerId) {
+    // TODO Impl
+    return ResponseEntity.ok(BeerDto.builder().build());
+  }
+
+  @PostMapping
+  public ResponseEntity saveBeer(@RequestBody BeerDto beerDto) throws URISyntaxException {
+    // TODO Impl
+    return ResponseEntity.created(new URI("")).build();
+  }
+
+  @PutMapping("/{beerId}")
+  public ResponseEntity updateBeerById(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+    // TODO Impl
+    return ResponseEntity.noContent().build();
+  }
+}
