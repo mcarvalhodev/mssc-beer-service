5fd31078fe85c3aaa46dafd325c5891e7be12e1c
Marcelo Santos <masantos.itlean@sbt.com.br>
2020-04-14 15:07:26 -0300

    adding controller tests - closes #1

---
+++ src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,54 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+  @Autowired MockMvc mockMvc;
+
+  @Autowired ObjectMapper objectMapper;
+
+  @Test
+  void getBeerById() throws Exception {
+    mockMvc
+        .perform(
+            get("/api/v1/beer/" + UUID.randomUUID().toString())
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  void saveBeer() throws Exception {
+    BeerDto beerDto = BeerDto.builder().build();
+    String beerJson = objectMapper.writeValueAsString(beerDto);
+    mockMvc
+        .perform(post("/api/v1/beer").contentType(MediaType.APPLICATION_JSON).content(beerJson))
+        .andExpect(status().isCreated());
+  }
+
+  @Test
+  void updateBeerById() throws Exception {
+
+    BeerDto beerDto = BeerDto.builder().build();
+    String beerJson = objectMapper.writeValueAsString(beerDto);
+
+    mockMvc
+        .perform(
+            put("/api/v1/beer/" + UUID.randomUUID().toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerJson))
+        .andExpect(status().isNoContent());
+  }
+}
