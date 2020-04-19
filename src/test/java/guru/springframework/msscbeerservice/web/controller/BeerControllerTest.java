bf289b4c5c4d2988d961f6a219c86b1f40800d4d
Marcelo Santos <masantos.itlean@sbt.com.br>
2020-04-19 10:59:14 -0300

    83. Fix broken tests - closes #8

--- src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -2,12 +2,14 @@ package guru.springframework.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -20,6 +22,15 @@ class BeerControllerTest {
 
   @Autowired ObjectMapper objectMapper;
 
+  static BeerDto getValidBeer() {
+    return BeerDto.builder()
+        .beerName("My bear")
+        .beerStyle(BeerStyleEnum.ALE)
+        .price(new BigDecimal("2.99"))
+        .upc(123123123123L)
+        .build();
+  }
+
   @Test
   void getBeerById() throws Exception {
     mockMvc
@@ -31,8 +42,7 @@ class BeerControllerTest {
 
   @Test
   void saveBeer() throws Exception {
-    BeerDto beerDto = BeerDto.builder().build();
-    String beerJson = objectMapper.writeValueAsString(beerDto);
+    String beerJson = objectMapper.writeValueAsString(getValidBeer());
     mockMvc
         .perform(post("/api/v1/beer").contentType(MediaType.APPLICATION_JSON).content(beerJson))
         .andExpect(status().isCreated());
@@ -41,8 +51,7 @@ class BeerControllerTest {
   @Test
   void updateBeerById() throws Exception {
 
-    BeerDto beerDto = BeerDto.builder().build();
-    String beerJson = objectMapper.writeValueAsString(beerDto);
+    String beerJson = objectMapper.writeValueAsString(getValidBeer());
 
     mockMvc
         .perform(
