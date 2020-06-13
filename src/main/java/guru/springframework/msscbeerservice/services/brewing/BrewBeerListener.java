abc94ae6bda5d90971d5753218106ba1d51c2eb4
Marcelo Santos <masantos.itlean@sbt.com.br>
2020-06-13 13:21:37 -0300

    Update parent BOM version - closes #21

--- src/main/java/guru/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
+++ src/main/java/guru/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
@@ -9,9 +9,12 @@ import guru.springframework.msscbeerservice.repository.BeerRepository;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
@@ -19,16 +22,22 @@ import org.springframework.stereotype.Service;
 public class BrewBeerListener {
 
   private final BeerRepository beerRepository;
-  private final JmsTemplate jmsTemplate;
+    private final JmsTemplate jmsTemplate;
+    private final RestTemplate restTemplate;
 
   @JmsListener(destination = JmsConfig.BREWING_REQUEST_QUEUE)
   public void listen(BrewBeerEvent event) {
-    BeerDto beerDto = event.getBeerDto();
-    Beer beer =
-            beerRepository.findById(beerDto.getId()).orElseThrow(ResourceNotFoundException::new);
-    beerDto.setQuantityOnHand(beer.getQuantityToBrew());
-    NewInventoryEvent newInventoryEvent = new NewInventoryEvent(beerDto);
-    log.debug("Brewed beer " + beer.getMinOnHand() + ": QOH: " + beerDto.getQuantityOnHand());
-    jmsTemplate.convertAndSend(JmsConfig.NEW_INVENTORY_QUEUE, newInventoryEvent);
+      BeerDto beerDto = event.getBeerDto();
+      Beer beer =
+              beerRepository.findById(beerDto.getId()).orElseThrow(ResourceNotFoundException::new);
+      beerDto.setQuantityOnHand(beer.getQuantityToBrew());
+      NewInventoryEvent newInventoryEvent = new NewInventoryEvent(beerDto);
+      log.debug("Brewed beer " + beer.getMinOnHand() + ": QOH: " + beerDto.getQuantityOnHand());
+      // jmsTemplate.convertAndSend(JmsConfig.NEW_INVENTORY_QUEUE, newInventoryEvent);
+      restTemplate.exchange(
+              "http://localhost:8082/api/v1/beer",
+              HttpMethod.POST,
+              new HttpEntity<>(newInventoryEvent.getBeerDto()),
+              BeerDto.class);
   }
 }
