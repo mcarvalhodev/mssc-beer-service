e6f4bcae629b950770119b5da26ccb3f5cf96592
Marcelo Santos <masantos.itlean@sbt.com.br>
2020-08-04 18:10:32 -0300

    Configuring RESTTemplate for HTTP basic authentication

--- src/main/java/guru/springframework/msscbeerservice/services/InventoryServiceImpl.java
+++ src/main/java/guru/springframework/msscbeerservice/services/InventoryServiceImpl.java
@@ -2,9 +2,10 @@ package guru.springframework.msscbeerservice.services;
 
 import guru.springframework.msscbeerservice.config.MsscConfigurationProperties;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -17,7 +18,6 @@ import java.util.Objects;
 import java.util.UUID;
 
 @Profile("!local-discovery")
-@RequiredArgsConstructor
 @Slf4j
 @Service
 public class InventoryServiceImpl implements InventoryService {
@@ -25,6 +25,15 @@ public class InventoryServiceImpl implements InventoryService {
   public static final String INVENTORY_PATH = "/api/v1/beer/{beerId}/inventory";
   private final MsscConfigurationProperties properties;
   private final RestTemplate restTemplate;
+
+  public InventoryServiceImpl(
+      MsscConfigurationProperties properties,
+      RestTemplateBuilder builder,
+      @Value("${guru.inventory-user}") String inventoryUser,
+      @Value("${guru.inventory-password}") String inventoryPassword) {
+    this.properties = properties;
+    this.restTemplate = builder.basicAuthentication(inventoryUser, inventoryPassword).build();
+  }
 
   @Override
   public Integer getOnHandInventory(UUID beerId) {
